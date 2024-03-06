29fb3a5bf51dfdbc9559d8558b1da0748a3fa649
LipeDorrio <felipejohnjohn@gmail.com>
2024-03-06 17:13:49 -0300

    First class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
-import java.util.Locale;
+import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		System.out.println("Hello World");
+		Position pos = new Position(3,5);
+		
+		System.out.println(pos);
 	}
 
 }
